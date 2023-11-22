135ec82f4766bc94b249671ef6460aca266f0cf6
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-11-22 21:14:44 +0900

    Update contents.js

--- scripts/contents.js
+++ scripts/contents.js
@@ -1,28 +1,18 @@
 if (location.host === "www.youtube.com") {
-  let video;
-  let retry = 0;
-  const MAX_RETRY = 5;
   function handle() {
-    const content = document.querySelectorAll("[class^='ytp-ad-text']");
-    const first = content[0] || false;
-    if (first) {
-      const selector = document.querySelectorAll("[id^='skip-button']");
-      const duration = video.duration || 999;
-      video.currentTime = duration;
-      if (selector) {
-        for (const select of selector) {
+    if (location.pathname === "/watch") {
+      const content = document.querySelectorAll("[class^='ytp-ad-text']");
+      const first = content[0] || false;
+      if (first) {
+        const select = document.querySelector("[id^='skip-button']");
+        const video = document.querySelector('video.html5-main-video');
+        video.currentTime = video.duration || 999;
+        if (select) {
           const button = select.querySelector("button");
           button && button.click();
         }
       }
     }
   }
-  function waitVideoLoad() {
-    (location.pathname === "/watch" && document.readyState === "complete") ?
-      (video = document.querySelector('video.html5-main-video')) ?
-        (setInterval(handle, 800)) :
-        ((retry < MAX_RETRY) ? (retry++, setTimeout(waitVideoLoad, 500)) : null) :
-      setTimeout(waitVideoLoad, 1000);
-  }
-  waitVideoLoad();
+  setInterval(handle, 800);
 }
