937362468e0d2eb9e066749f69c6bbf152b34278
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-11-23 19:51:04 +0900

    Update contents.js

--- scripts/contents.js
+++ scripts/contents.js
@@ -2,11 +2,11 @@ if (location.host === "www.youtube.com") {
   var video;
   const handle = () => {
     if (location.pathname === "/watch") {
-      video = video || document.querySelector('video.html5-main-video');
       const button = document.querySelector("[id^='skip-button'] button");
       const content = document.querySelectorAll("[class^='ytp-ad-text']");
-      if (content[0] && (video || button)) {
-        video.currentTime = video.duration || 999;
+      if (content[0] || button) {
+        video = video || document.querySelector('video.html5-main-video');
+        video ? (video.currentTime = video.duration || 999) : null;
         button && button.click();
       }
     } else {
